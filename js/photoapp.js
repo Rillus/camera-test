be08b49781f5195151b0357d06e6ed1a3c8eb08c
Rillus <me@ariley.me>
2013-07-24 17:23:42 +0100

    added upload function

--- js/photoapp.js
+++ js/photoapp.js
@@ -9,7 +9,7 @@ var photoApp = {
         this.bindEvents();
     },
     // Bind Event Listeners
-    //
+
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
@@ -17,7 +17,7 @@ var photoApp = {
         document.addEventListener('deviceready', photoApp.onDeviceReady, false);
     },
     // deviceready Event Handler
-    //
+
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
@@ -33,56 +33,79 @@ var photoApp = {
         // console.log(imageData);
 
         // Get image handle
-        //
         var smallImage = document.getElementById('smallImage');
 
         // Unhide image elements
-        //
         smallImage.style.display = 'block';
 
         // Show the captured photo
         // The inline CSS rules are used to resize the image
-        //
         smallImage.src = "data:image/jpeg;base64," + imageData;
     },
+    
     // Called when a photo is successfully retrieved
-    //
     onPhotoURISuccess: function(imageURI) {
         // Uncomment to view the image file URI
         // console.log(imageURI);
 
         // Get image handle
-        //
         var smallImage = document.getElementById('smallImage');
 
         // Unhide image elements
-        //
         smallImage.style.display = 'block';
 
         // Show the captured photo
         // The inline CSS rules are used to resize the image
-        //
         smallImage.src = imageURI;
     },
+    
     // A button will call this function
-    //
     capturePhoto: function () {
         // Take picture using device camera and retrieve image as base64-encoded string
         navigator.camera.getPicture(photoApp.onPhotoDataSuccess, photoApp.onFail, { quality: 50,
             destinationType: this.settings.destinationType.DATA_URL });
     },
+    
     // A button will call this function
-    //
     getPhoto: function(source) {
         // Retrieve image file location from specified source
         navigator.camera.getPicture(photoApp.onPhotoURISuccess, photoApp.onFail, { quality: 50,
             destinationType: photoApp.settings.destinationType.FILE_URI,
             sourceType: source });
     },
+    
     // Called if something bad happens.
-    //
     onFail: function (message) {
         alert('Failed because: ' + message);
-    }
+    },
+
+    uploadPhoto: function (imageURI) {
+        var options = new FileUploadOptions(),
+            photoApp = this;
+        options.fileKey="file";
+        options.fileName=imageURI.substr(imageURI.lastIndexOf('/')+1);
+        options.mimeType="image/jpeg";
+
+        var params = {};
+        params.value1 = "test";
+        params.value2 = "param";
+
+        options.params = params;
+
+        var ft = new FileTransfer();
+        ft.upload(imageURI, encodeURI(post.settings.baseUrl+'post/image'), photoApp.win, photoApp.fail, options);
+    },
+
+    win: function(r) {
+        console.log("Code = " + r.responseCode);
+        console.log("Response = " + r.response);
+        console.log("Sent = " + r.bytesSent);
+    },
+
+    fail: function(error) {
+        alert("An error has occurred: Code = " + error.code);
+        console.log("upload error source " + error.source);
+        console.log("upload error target " + error.target);
+    },
 };
 photoApp.initialize();
