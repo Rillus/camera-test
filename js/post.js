69346ff515d2e5635718627c4c0a744064579e65
Rillus <me@ariley.me>
2013-07-24 13:32:27 +0100

    fixed up login. started on status

---
+++ js/post.js
@@ -0,0 +1,74 @@
+var post = {
+
+    settings : {
+        baseUrl : "http://172.19.147.50/sect.tv/index.php/",
+        username : "",
+        userId : "",
+        errorMsg : ""
+    },
+    // Application Constructor
+    init: function() {
+        this.bindEvents();
+        this.errorCheck();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        var sett = this.settings,
+            post = this;
+
+        //add event handler to the `submit` event for your login form
+        $(document).on('submit', 'form', function(event) {
+            event.preventDefault();
+            validateAll();
+
+            if ($('.alert').length == 0){
+                console.log('user clicky', $(this));
+
+                //send a post request to the web-service
+                var formAction = $(this).data('action');
+                post.postData(sett.baseUrl+formAction, $(this).serialize());
+            }
+        }, event);
+    },
+    postData: function(url, data){
+        var sett = this.settings,
+            post = this;
+        
+        console.log('posting', data)
+        
+        $.post(url, data, function (response) {
+            //parse the response string into an object
+            response = $.parseJSON(response);
+
+            console.log('received resposne', response);
+
+            //check if the authorization was successful or not
+            if (response.status == 'success') {
+                sett.username = response.username;
+                sett.userId = response.userid;
+                sett.errorMsg = "";
+                
+                $.mobile.changePage('#share', "slide");
+            } else if (response.status == 'fail') {
+                $.mobile.changePage('#index');
+                post.errorCheck("Bad username/password combo.");
+            } else {
+                post.errorCheck("An unspecified error occurred. Sorry I can't be more useful.");
+            }
+        });
+
+    },
+    errorCheck: function(message) {
+        var sett = this.settings;
+
+        sett.errorMsg = message;
+
+        if (message != ""){
+            $('.errorHolder').html(message);
+        }
+    }
+};
+post.init();
